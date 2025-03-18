a392f6328a448d0ae49fda36a0e3d1e4f58b6a0c
Xin Jin <124172716+JinXins@users.noreply.github.com>
2025-03-19 01:10:57 +0800

    Update __init__.py

--- openmixup/datasets/data_sources/__init__.py
+++ openmixup/datasets/data_sources/__init__.py
@@ -1,21 +1,11 @@
 from .cifar import CIFAR10, CIFAR100, CIFAR_C
 from .image_list import ImageList
 from .imagenet import ImageNet
-<<<<<<< HEAD
 from .mnist import MNIST, FMNIST, KMNIST, USPS
-from .palm_vein import Palm_Vein
 from .cars import Cars
-=======
-from .mnist import MNIST, FMNIST, KMNIST, RCFMNIST, USPS
->>>>>>> db2c4ac (update some vit-based mixup methods and fix robustness eval tasks)
 
 __all__ = [
     'CIFAR10', 'CIFAR100', 'CIFAR_C',
     'ImageList', 'ImageNet',
-<<<<<<< HEAD
-    'MNIST', 'FMNIST', 'KMNIST', 'USPS',
-    'Palm_Vein', 'Cars'
-=======
-    'MNIST', 'FMNIST', 'KMNIST', 'RCFMNIST', 'USPS',
->>>>>>> db2c4ac (update some vit-based mixup methods and fix robustness eval tasks)
+    'MNIST', 'FMNIST', 'KMNIST', 'USPS', 'Cars'
 ]
